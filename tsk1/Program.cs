0c704c475888511537341a3f95c30a6d33f2ab16
Maxim <you@example.com>
2022-08-27 19:04:59 +0300

    Com1

--- tsk1/Program.cs
+++ tsk1/Program.cs
@@ -16,8 +16,8 @@ int[,] GetArray(int m, int n, int minValue, int maxValue)
 
 void PrintArray(int[,] array)
 {
-    for (int i = 0; i<m; i++) {
-        for(int j = 0; j<n; j++)
+    for (int i = 0; i<array.GetLength(0); i++) {
+        for(int j = 0; j<array.GetLength(1); j++)
            Console.Write($"{array[i, j]} ");
         Console.WriteLine();
     }
