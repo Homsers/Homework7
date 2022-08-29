5581b3d6aac17b9bd58dc9764c7d9e91393ceda9
Maxim <you@example.com>
2022-08-29 16:56:17 +0300

    com01

--- tsk1/Program.cs
+++ tsk1/Program.cs
@@ -3,25 +3,30 @@ int m = int.Parse(Console.ReadLine());
 Console.Write("n=");
 int n = int.Parse(Console.ReadLine());
 
-int[,] GetArray(int m, int n, int minValue, int maxValue)
+double[,] GetArray(int m, int n, int minValue, int maxValue)
+
 {
-    int[,] result = new int [m, n];
+{
+    double[,] arrays = new double [m, n];
     
     for (int i = 0; i<m; i++)
         for(int j=0; j<n; j++)
-           result[i, j] = new Random().Next(minValue, maxValue+1);
+           arrays[i, j] = Convert.ToDouble(new Random().Next(minValue, maxValue));
 
-    return result;
+    return arrays;
+    
 }
-
-void PrintArray(int[,] array)
+}
+void PrintArray(double[,] array)
 {
     for (int i = 0; i<array.GetLength(0); i++) {
         for(int j = 0; j<array.GetLength(1); j++)
-           Console.Write($"{array[i, j]} ");
+           Console.Write($"[{array[i, j]}]");
+        Console.WriteLine();
         Console.WriteLine();
     }
 }
 
-int[,] myArray = GetArray(m, n, 0, 100);
+
+double[,] myArray = GetArray(m, n, -10, 10);
 PrintArray(myArray);
