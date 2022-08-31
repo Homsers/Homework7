bd39eda2d25bc07aa06a82cdf7183f6c33641ebb
Maxim <you@example.com>
2022-08-31 16:27:19 +0300

    1com

--- tsk2/Program.cs
+++ tsk2/Program.cs
@@ -2,6 +2,10 @@
 int m = int.Parse(Console.ReadLine());
 Console.Write("n=");
 int n = int.Parse(Console.ReadLine());
+Console.Write("Enter a first number of element=");
+int s = int.Parse(Console.ReadLine())-1;
+Console.Write("Enter a second number of element=");
+int c = int.Parse(Console.ReadLine())-1;
 
 int[,] GetArray(int m, int n, int minValue, int maxValue)
 {
@@ -9,8 +13,7 @@ int[,] GetArray(int m, int n, int minValue, int maxValue)
     
     for (int i = 0; i<m; i++)
         for(int j=0; j<n; j++)
-           result[i, j] = new Random().Next(minValue, maxValue+1);
-
+           result[i, j] = new Random().Next(minValue, maxValue);
     return result;
 }
 
@@ -18,10 +21,19 @@ void PrintArray(int[,] array)
 {
     for (int i = 0; i<array.GetLength(0); i++) {
         for(int j = 0; j<array.GetLength(1); j++)
-           Console.Write($"{array[i, j]} ");
+        Console.Write($"{array[i, j]} ");
         Console.WriteLine();
+        if (s == array.GetLength(0)-1 | c == array.GetLength(1))
+        Console.WriteLine("Number of element is {0}", array[s,c]);
     }
 }
 
-int[,] myArray = GetArray(m, n, 0, 100);
+
+int[,] myArray = GetArray(m, n, 0, 10);
 PrintArray(myArray);
+Console.WriteLine("Number of element is {0}", myArray[s,c]);
+
+
+
+
+
---
+++ tsk3/Program.cs
@@ -0,0 +1,46 @@
+﻿Console.Write("m=");
+int m = int.Parse(Console.ReadLine());
+Console.Write("n=");
+int n = int.Parse(Console.ReadLine());
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int [m, n];
+    
+    for (int i = 0; i<m; i++)
+        for(int j=0; j<n; j++)
+           result[i, j] = new Random().Next(minValue, maxValue+1);
+
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i<array.GetLength(0); i++) {
+        for(int j = 0; j<array.GetLength(1); j++)
+           Console.Write($"{array[i, j]} ");
+        Console.WriteLine();
+    }
+}
+void PrintArray2(int[,] array2)
+{
+for (int j = 0; j < array2.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < array2.GetLength(0); i++)
+    {
+        sum += array2[i, j];
+    }
+    Console.Write($"[{Math.Round(( sum / array2.GetLength(0)))}] ");
+
+
+}
+Console.ReadLine();
+
+}
+int[,] myArray = GetArray(m, n, 0, 9);
+
+PrintArray(myArray);
+PrintArray2(myArray);
+
+
